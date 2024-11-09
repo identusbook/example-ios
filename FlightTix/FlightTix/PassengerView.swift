c80b4846fb6c93bb2807d70c42cbce374f34ae7b
Jon Bauer <jon@covelop.org>
2024-11-09 14:34:16 -0800

    Flight list

--- FlightTix/FlightTix/PassengerView.swift
+++ FlightTix/FlightTix/PassengerView.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+enum Flights: String, CaseIterable, Identifiable {
+    case atl2scl = "ATL -> SCL 20:00 Mar 23, 2025",
+         ams2vie = "AMS -> VIE 09:30 May 29, 2025",
+         sfo2hnd = "SFO -> HND 11:00 Oct 25, 2025"
+    var id: Self { self }
+}
+
 struct PassengerView: View {
+    
+    @State private var selectedFlight: Flights = .atl2scl
+    
+    private func showProfile() {}
+    
     var body: some View {
-        Text("Hello, Passenger!")
+        ZStack {
+            VStack {
+                Text("Choose Flight:")
+                
+                Picker("Flights", selection: $selectedFlight) {
+                    ForEach(Flights.allCases) { flight in
+                        Text(flight.rawValue)
+                    }
+                }
+                
+                Button  {
+                    showProfile()
+                } label: {
+                    Text("Purchase Ticket")
+                }
+                .buttonStyle(.bordered)
+            }
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Button  {
+                        showProfile()
+                    } label: {
+                        Image(systemName: "person.crop.circle.fill")
+                    }
+                    .padding(.trailing, 24)
+                }
+                .padding(.top, 8)
+                Spacer()
+            }
+            
+        }
     }
 }
 
