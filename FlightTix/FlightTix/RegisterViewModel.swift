2f5858cfce4bb6f60a23b3aba348efe50c944d7c
Jon Bauer <jon@covelop.org>
2025-01-05 16:52:00 -0800

    Save successful invitationId as connectionId, add ProgressView to loading screen

--- FlightTix/FlightTix/RegisterViewModel.swift
+++ FlightTix/FlightTix/RegisterViewModel.swift
@@ -27,37 +27,30 @@ class RegisterViewModel: ObservableObject {
              The Issuer then uses the /issue-credentials/records/{recordId}/issue-credential endpoint to issue the credential, which gets sent to the Holder via DIDComm. The Holder receives the credential, and the protocol is complete.
              */
             
+            // Get IssuerDID and verify it's been published.
             guard let issuerDID = Identus.shared.readIssuerDIDFromKeychain() else { return }
-            
             guard let shortFormIssuerDID = try await Identus.shared.didShortForm(from: issuerDID) else { return }
-            
             guard try await Identus.shared.verifyIssuerDIDIsPublished(shortOrLongFormDID: shortFormIssuerDID.string) else { return }
             
             // Get ConnectionId
-            guard let currentConnectionId = Identus.shared.readConnectionIdFromKeychain() else {
-                print("ConnectionID not found")
-                return
-            }
+            guard let currentConnectionId = Identus.shared.readConnectionIdFromKeychain() else { return }
             
-            return // remove after connectionId is stored correctly
             do {
                 let credentialOffer = try await Identus.shared.createCredentialOffer(request: CreateCredentialOfferRequest(
-                                                                                                                           label: "FlightTixiOS-CloudAgent",
-                                                                                                                           validityPeriod: 3600,
-                                                                                                                           //schemaId: "http://localhost/cloud-agent/schema-registry/schemas/5314fb38-b9ba-3871-bba1-7146d31b790a",
-                                                                                                                           credentialFormat: "JWT",
-                                                                                                                           claims: PassportClaims(name: "Jon Bauer",
-                                                                                                                                                  did: "1234567890",
-                                                                                                                                                  dateOfIssuance: "2025-01-01T05:35:56.993032Z",
-                                                                                                                                                  passportNumber: "987654322",
-                                                                                                                                                  dob: "2025-01-01T05:35:56.993032Z"),
-                                                                                                                           automaticIssuance: true,
-                                                                                                                           issuingDID: shortFormIssuerDID.string,
-                                                                                                                           //issuingKid: "kid1",
-                                                                                                                           //connectionId: currentConnectionId.lowercased(),
-                                                                                                                           connectionId: "d9a9e0a3-fa55-4405-aef7-1ef04d77fa2b",
-                                                                                                                           goalCode: "issue-vc",
-                                                                                                                           goal: "To issue a Passport Verifiable Credential")
+                    label: "FlightTixiOS-CloudAgent",
+                    validityPeriod: 3600,
+                    credentialFormat: "JWT",
+                    claims: PassportClaims(name: "Jon Bauer",
+                                           did: "1234567890",
+                                           dateOfIssuance: "2025-01-01T05:35:56.993032Z",
+                                           passportNumber: "987654322",
+                                           dob: "2025-01-01T05:35:56.993032Z"),
+                    automaticIssuance: true,
+                    issuingDID: shortFormIssuerDID.string,
+                    //issuingKid: "kid1",
+                    connectionId: currentConnectionId,
+                    goalCode: "issue-vc",
+                    goal: "To issue a Passport Verifiable Credential")
                 )
                 print("We should have created a CreditentialOffer at this point \(String(describing: credentialOffer))")
                
